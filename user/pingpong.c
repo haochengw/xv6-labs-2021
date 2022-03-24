8f3339fb55a063d69cd96954d14030a4344fc523
haochengw <hihaochengw@outlook.com>
2022-03-24 16:23:43 +0800

    update lab1-pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,35 +2,34 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+# define READEND 0
+# define WRITEEND 1
 
 int main(int argc, char *argv[]) {
-    if (argc != 1) {
-        fprintf(2, "error argument count");
-        exit(1);
-    }
-    int childPipe[2]; // p[0] -> in, p[1] -> out
+    char buf[1];
+    buf[0] = 'a';
+
+    int childPipe[2]; 
     int parentPipe[2];
     pipe(childPipe);
     pipe(parentPipe);
 
-
-    // 发送
-    char buf[512];
-    buf[0] = 'a';
-    write(childPipe[1], buf, 1);
-
     if (fork() == 0) {
-        int pid = getpid();
-        close(childPipe[1]);
-        int receivedBytesNum = read(childPipe[0], buf, 1);
-        printf("%d: received ping\n", pid);
-        write(parentPipe[1], buf, receivedBytesNum);
-        exit(0);
-    } else {
-        read(parentPipe[0], buf, 1);
-        close(parentPipe[1]);
-        int pid = getpid();
-        printf("%d: received pong\n", pid);
-        exit(0);
+        close(childPipe[WRITEEND]);
+        close(parentPipe[READEND]);
+        read(childPipe[READEND], buf, 1);
+        printf("%d: received ping\n", getpid());
+        close(childPipe[READEND]);
+        write(parentPipe[WRITEEND], buf, 1);
+        close(parentPipe[WRITEEND]);
+    } else { // 父进程，发送数据后读取数据
+        close(parentPipe[WRITEEND]);
+        close(childPipe[READEND]);
+        write(childPipe[WRITEEND], buf, 1);
+        close(childPipe[WRITEEND]);
+        read(parentPipe[READEND], buf, 1);
+        close(parentPipe[READEND]);
+        printf("%d: received pong\n", getpid());
     }
+    exit(0);
 }
